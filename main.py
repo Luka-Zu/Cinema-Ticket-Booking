3beb0ff6034230f467b8d922c9799d1a0c64cecc
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-29 00:08:14 +0400

    monaxazi

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-# User:
-# name
-# buy(seat, card)
 
 class User:
-    pass
+    def __init__(self, name):
+        self.name=name
 
+    def buy(self, seat, card):
+        pass
 
-# Seat:
-# database
-# seat_id
-# price
-# is_free()
-# occupy()
 
 class Seat:
-    pass
+    database = "cinema.db"
+
+    def __init__(self, seat_id):
+        self.seat_id = seat_id
+
+    def get_price(self):
+        pass
+
+    def is_free(self):
+        pass
+
+    def occupy(self):
+        pass
+
 
 
 
@@ -27,7 +34,16 @@ class Seat:
 # validate(price)
 
 class Card:
-    pass
+    database = "banking.db"
+    
+    def __init__(self, type, number, cvc, holder):
+        self.holder = holder
+        self.cvc = cvc
+        self.number = number
+        self.type = type
+
+    def validate(self, price):
+        pass
 
 
 # Ticket:
@@ -38,4 +54,12 @@ class Card:
 # to_pdf(path)
 
 class Ticket:
-    pass
+
+    def __init__(self, user, price, seat_number):
+        self.seat_number = seat_number
+        self.price = price
+        self.user = user
+
+    def to_pdf(self):
+        pass
+
