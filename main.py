cac072f2bec3058109c77085d5081594eacb9ca1
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-30 00:01:44 +0400

    implemented seat class.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import sqlite3
+
 
 class User:
     def __init__(self, name):
-        self.name=name
+        self.name = name
 
     def buy(self, seat, card):
         pass
@@ -11,31 +13,45 @@ class Seat:
     database = "cinema.db"
 
     def __init__(self, seat_id):
-        self.seat_id = seat_id
+        self.__seat_id = seat_id
+        connection_with_database = sqlite3.connect(database=self.database)
+        cursor = connection_with_database.cursor()
+        cursor.execute(f"""
+        SELECT  * FROM "Seat" WHERE seat_id = '{self.__seat_id}'
+        """)
+        result = cursor.fetchall()
+
+        self.__price = result[0][2]
+        self.__is_available = (result[0][1] == 0)
+
+        connection_with_database.close()
 
     def get_price(self):
-        pass
+        return self.__price
 
     def is_free(self):
-        pass
+        return self.__is_available
 
     def occupy(self):
-        pass
+        if self.is_free():
+            connection_with_database = sqlite3.connect(database=self.database)
+            connection_with_database.execute(f"""
+                        UPDATE "Seat" SET "taken"=1 WHERE seat_id='{self.__seat_id}'    
+                        """)
+            self.__is_available = False
+            connection_with_database.commit()
+            connection_with_database.close()
+            return True
+        else:
+            print("Seat is already occupied! ")
+            return False
 
 
 
-
-# Card:
-# database
-# type
-# number
-# cvc
-# holder
-# validate(price)
 
 class Card:
     database = "banking.db"
-    
+
     def __init__(self, type, number, cvc, holder):
         self.holder = holder
         self.cvc = cvc
@@ -46,13 +62,6 @@ class Card:
         pass
 
 
-# Ticket:
-# id
-# user
-# price
-# seat
-# to_pdf(path)
-
 class Ticket:
 
     def __init__(self, user, price, seat_number):
@@ -62,4 +71,11 @@ class Ticket:
 
     def to_pdf(self):
         pass
+
+
+a = Seat('A1')
+
+print(a.get_price())
+print(a.is_free())
+print(a.occupy())
 
